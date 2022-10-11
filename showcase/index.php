2b13214fe2ecdd391f3e0c0630e9eb68c38c5d93
azuri <pribora2006@mail.ru>
2022-10-11 11:09:25 +0600

    showcase index

---
+++ showcase/index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+
+<html lang="en" crosspilot> 
+ <head>
+  <link rel="icon" type="image/x-icon" href="https://azuretium.space/imgs/favicon.png">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <meta charset="UTF-8">
+  <meta name="description" content="My works. Etazhi, gm_ame, photoscan office etc.">
+  <meta name="Keywords" content="Azuri, heut, heutick">
+  <meta name="robots" content="noindex">
+  <link rel="stylesheet" href="../style/style.css">
+  <title> showcase </title> 
+
+<!-- meta -->
+<meta name="theme-color" content="#000000">
+<meta property="og:site_name" content="azuretium.space">
+<meta property="og:title" content="Showcase, images, works etc.">
+<meta property="og:url" content="https://azuretium.space">
+<meta property="og:type" content="website">
+<meta property="og:image" content="https://azuretium.space/imgs/meta.png">
+<meta itemprop="name" content="Showcase, images, works etc.">
+<meta itemprop="url" content="https://azuretium.space">
+<meta itemprop="thumbnailUrl" content="https://azuretium.space/imgs/meta.png">
+<link rel="image_src" href="https://azuretium.space/imgs/meta.png">
+<meta itemprop="image" content="https://azuretium.space/imgs/meta.png">
+
+<meta name="twitter:title" content="Showcase, images, works etc.">
+<meta name="twitter:url" content="https://azuretium.space">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:image" content="https://azuretium.space/imgs/meta.png">
+<meta name="description" content="My works.">
+
+  </head>
+ <body>
+    
+  <nav class="nav">
+          
+    <div class="flexing">
+      <a title="azuretium.space" href="https://azuretium.space" class="titletoplink">
+        <div class="logo"></div> <h1 class="maintitle">Azuretium </h1>
+      </a>
+      
+    </div>
+        <nav class="defolt-menu">
+            <ul>
+            <li><a class="button" href="https://azuretium.space#about">About</a></li>
+            <li><a class="button" href="https://azuretium.space#skills">Skills</a></li>
+            <li><a class="button" href="https://azuretium.space#projects">Projects</a></li>
+            <li><a class="button" href="https://azuretium.space/blogs">Blogs</a></li>
+            </ul>
+          </nav>
+
+        <div id="hamburger-icon" onclick="toggleMobileMenu(this)">
+          <div class="bar1"></div>
+          <div class="bar2"></div>
+          <div class="bar3"></div>
+          <ul class="mobile-menu">
+            <li><a class="button" href="https://azuretium.space#about">About</a></li>
+            <li><a class="button" href="https://azuretium.space#skills">Skills</a></li>
+            <li><a class="button" href="https://azuretium.space#projects">Projects</a></li>
+            <li><a class="button" href="https://azuretium.space/blogs">Blogs</a></li>
+          </ul>
+        </div>
+
+</nav>
+
+
+<div id="mid-showcase">
+  <div class="twi-top">
+    <h2 class="title-showcase">map - ame - gmod</h2>
+    <img src="https://azuretium.space/imgs/showcase/ame1.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/ame2.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/ame3.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/ame4.jpg" class="image-showcase">
+  </div>
+
+  <div class="twi">
+    <h2 class="title-showcase">map - etazhi - gmod</h2>
+    <img src="https://azuretium.space/imgs/showcase/2.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/enazhi2.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/etazhi3.jpg" class="image-showcase">
+  </div>
+
+  <div class="twi">
+    <h2 class="title-showcase">map - lost coast (edit) - gmod</h2>
+    <img src="https://azuretium.space/imgs/showcase/los4.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/los.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/los1.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/los3.jpg" class="image-showcase">
+  </div>
+
+  <div class="twi">
+    <h2 class="title-showcase">map - rndevs office - gmod</h2>
+    <img src="https://azuretium.space/imgs/showcase/1.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/office.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/office2.jpg" class="image-showcase">
+  </div>
+
+  <div class="twi">
+    <h2 class="title-showcase">map - rp streets (WIP) - s&box</h2>
+    <img src="https://azuretium.space/imgs/showcase/6.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/strets.jpg" class="image-showcase">
+  </div>
+
+  <div class="twi">
+    <h2 class="title-showcase">map - new york (WIP) - s&box</h2>
+    <img src="https://azuretium.space/imgs/showcase/8.png" class="image-showcase">
+  </div>
+
+  <div class="twi">
+    <h2 class="title-showcase">map - voxel house (WIP) - s&box</h2>
+    <img src="https://azuretium.space/imgs/showcase/7.jpg" class="image-showcase">
+  </div>
+
+  <div class="twi">
+    <h2 class="title-showcase">map - zavod (contributing) - gmod</h2>
+    <img src="https://azuretium.space/imgs/showcase/los2.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/zavod.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/zavod2.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/zavod3.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/zavod4.jpg" class="image-showcase">
+  </div>
+
+  <div class="twi">
+    <h2 class="title-showcase">map - escapism (contributing) - gmod</h2>
+    <img src="https://azuretium.space/imgs/showcase/222.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/esca1.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/esca2.jpg" class="image-showcase">
+  </div>
+
+  <div class="twi">
+    <h2 class="title-showcase">map - sunset islands (old) - gmod</h2>
+    <img src="https://azuretium.space/imgs/showcase/3.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/sunset2.jpg" class="image-showcase">
+  </div>
+
+  <div class="twi">
+    <h2 class="title-showcase">map - last (old) - gmod</h2>
+    <img src="https://azuretium.space/imgs/showcase/4.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/last1.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/last3.jpg" class="image-showcase">
+  </div>
+
+  <div class="twi">
+    <h2 class="title-showcase">map - dream house (old) - gmod</h2>
+    <img src="https://azuretium.space/imgs/showcase/5.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/housecringe.jpg" class="image-showcase">
+  </div>
+
+  <div class="twi">
+    <h2 class="title-showcase">map - snow house (old) - gmod</h2>
+    <img src="https://azuretium.space/imgs/showcase/9.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/snowdom2.jpg" class="image-showcase">
+    <img src="https://azuretium.space/imgs/showcase/snowdom1.jpg" class="image-showcase">
+  </div>
+
+  
+</div>
+
+
+  <div class="bottom">
+  <div class="footer">
+  <div class="text-f">
+        <p class="bottext"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-copyright" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <circle cx="12" cy="12" r="9"></circle>
+            <path d="M14 9.75a3.016 3.016 0 0 0 -4.163 .173a2.993 2.993 0 0 0 0 4.154a3.016 3.016 0 0 0 4.163 .173"></path>
+         </svg> Azuri 2022 </p> 
+        <a href="https://github.com/azuri42069/heut42069.github.io/issues" class="reportbug"> <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bug" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M9 9v-1a3 3 0 0 1 6 0v1"></path>
+            <path d="M8 9h8a6 6 0 0 1 1 3v3a5 5 0 0 1 -10 0v-3a6 6 0 0 1 1 -3"></path>
+            <line x1="3" y1="13" x2="7" y2="13"></line>
+            <line x1="17" y1="13" x2="21" y2="13"></line>
+            <line x1="12" y1="20" x2="12" y2="14"></line>
+            <line x1="4" y1="19" x2="7.35" y2="17"></line>
+            <line x1="20" y1="19" x2="16.65" y2="17"></line>
+            <line x1="4" y1="7" x2="7.75" y2="9.4"></line>
+            <line x1="20" y1="7" x2="16.25" y2="9.4"></line>
+         </svg> Report a issue</a>
+    </div>
+  <div class="linkss">
+    
+  <a href="https://t.me/azuretium " title="my tg" target="_blank"><img src="https://azuretium.space/imgs/tg.png" class="button2"> </a> 
+  <a href="https://www.youtube.com/channel/UCt5FiVUoCsoMTIRHyP8Fx8A" title="my youtube" target="_blank"><img src="https://azuretium.space/imgs/youtube.png" class="button2"> </a>
+  <a href="https://www.moddb.com/members/azuretium" title="my moddb" target="_blank"><img src="https://azuretium.space/imgs/moddb.png" class="button2"> </a>
+  <a href="https://boosty.to/heutick" title="DOANT ME" target="_blank"><img src="https://azuretium.space/imgs/boosty.png" class="button2"> </a>
+  <a href="https://discordapp.com/users/449448506780811265/" title="My discord" target="_blank"> <img src="https://azuretium.space/imgs/discord.png" class="button2"> </a>
+  <a href="https://github.com/azuri42069" title="my github" target="_blank"><img src="https://azuretium.space/imgs/github.png" class="button2"> </a>
+  <a href="https://steamcommunity.com/profiles/76561198414640282" title="My steam" target="_blank"> <img src="https://azuretium.space/imgs/steam.png" class="button2"> </a>
+  </div>  
+</div>
+</div>
+
+<!-- scripts -->
+<script src="../scripts/script2.js"></script>
+</body>
+ </body>
+</html>
